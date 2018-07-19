9c701765813c1aa8d01d857d268ba9cf1b30f322
lkv123 <lakshmisoujanyakota@gmail.com>
2018-07-19 18:12:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def msg():
 @app.route('/sendmsg')
 def sendmsg():
     to = request.args.get('to')
+    print to
     msg = request.args.get('msg')
     token = request.args.get('token')
     if(str(token) != 'a'):
